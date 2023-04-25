a999b8a7b61ef0fc8efbcd1efce42339aff141dc
Turgay <turgaysozen@hotmail.com>
2023-04-26 01:18:08 +0300

    Fetch jobs from cache and return to client

--- handlers/jobs.go
+++ handlers/jobs.go
@@ -1,11 +1,22 @@
 package handlers
 
 import (
+	"bytes"
 	"context"
 	"net/http"
+
+	"github.com/remote-job-finder/utils/redis"
 )
 
 func HomeHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	keys, _ := redis.RedisClient.LRange(ctx, "categories", 0, -1).Result()
+	var jobBytes [][]byte
+	for _, key := range keys {
+		jobs, _ := redis.GetJobs(ctx, key)
+		jobBytes = append(jobBytes, jobs)
+	}
+
+	jsonBytes := bytes.Join(jobBytes, []byte("."))
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte("Jobs"))
+	w.Write(jsonBytes)
 }
