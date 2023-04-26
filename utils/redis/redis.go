c63771c096566fefe9017b6b0cf7c925b870665e
Turgay <turgaysozen@hotmail.com>
2023-04-26 20:47:42 +0300

    Change redis address for container

--- utils/redis/redis.go
+++ utils/redis/redis.go
@@ -11,7 +11,7 @@ var RedisClient *redis.Client
 
 func init() {
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	})
