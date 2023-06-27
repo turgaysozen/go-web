c99d266510c3a485ba8cd8a2130284d7bb24eaeb
Turgay <turgaysozen@hotmail.com>
2023-06-27 23:23:09 +0300

    Read data from db and apply structural changes

--- handlers/jobs.go
+++ handlers/jobs.go
@@ -5,49 +5,40 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/remote-job-finder/service/rss"
-	"github.com/remote-job-finder/utils/common"
+	"github.com/remote-job-finder/utils/db"
 	"github.com/remote-job-finder/utils/logger"
-	"github.com/remote-job-finder/utils/redis"
 )
 
-func JobsHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	keys, _ := redis.RedisClient.LRange(ctx, common.CategoriesKey, 0, -1).Result()
-	logger.Info.Println(
-		"Key fetched from redis and jobs are fething from the cache,",
-		"keys:", keys,
-	)
+func JobsHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, database *db.Database) {
+	allActiveJobsByCat, err := database.GetAllActiveJobsByCat()
+	if err != nil {
+		logger.Error.Println("An error occurred while getting all jobs, err:", err)
+	}
 
-	var jobSummary []rss.JobSummary
-	var jobs rss.JobSummary
-	for _, key := range keys {
-		jobData, _ := redis.GetJobs(ctx, key)
-
-		var job rss.Channel
-		err := json.Unmarshal(jobData, &job)
-
-		if err != nil {
-			logger.Error.Println("An error occurred while unmarshalling cache data, err:", err)
-		} else {
-			jobs.Jobs = []rss.JobsFields{}
-			for _, j := range job.Jobs {
-				jobFields := rss.JobsFields{
-					Title:    j.Title,
-					Company:  j.Company.Name,
-					Type:     j.Type,
-					Location: j.Region,
-					Date:     j.Date,
-					Logo:     j.Company.Logo,
-				}
-
-				jobs.Jobs = append(jobs.Jobs, jobFields)
+	var jobs rss.JobSummaryDTO
+	var jobSummary []rss.JobSummaryDTO
+	for _, cat := range allActiveJobsByCat {
+		jobs.Jobs = []rss.JobsFieldsDTO{}
+		for _, j := range cat.Jobs {
+			jobFields := rss.JobsFieldsDTO{
+				ID:       j.ID,
+				Title:    j.Title,
+				Company:  j.Company.Name,
+				Type:     j.Type,
+				Location: j.Region,
+				Date:     j.PubDate,
+				Logo:     j.Company.Logo,
 			}
+			jobs.Jobs = append(jobs.Jobs, jobFields)
 		}
-		jobs.Description = job.Description
+		jobs.CategoryName = cat.Name
+		jobs.CategoryID = cat.ID
 		jobSummary = append(jobSummary, jobs)
+		logger.Info.Println("Retrieving jobs by category name:", cat.Name, ", total:", len(jobs.Jobs))
 	}
 
 	jobsByte, _ := json.Marshal(jobSummary)
@@ -55,75 +46,92 @@ func JobsHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	w.Write(jobsByte)
 }
 
-func JobDetailsHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, slug string) {
-	logger.Info.Println("Handling slug:", slug)
+func JobDetailsHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, slug string, database *db.Database) {
+	splits := strings.Split(slug, "--")
+	logger.Info.Println("Getting single job for slug:", splits[0], "job ID:", splits[1])
 
-	slugArr := strings.Split(slug, "--")
-	jobData, _ := redis.GetJobs(ctx, slugArr[0])
-
-	var job rss.Channel
-	var jobDetaByte []byte
-	err := json.Unmarshal(jobData, &job)
-	if err == nil {
-		for _, job := range job.Jobs {
-			slug := createSlug(job.Title)
-			if slug == slugArr[1] {
-				job.Applicants = redis.GetJobApplicantCount(ctx, common.JobApplicantsCountKey, slug)
-				logger.Info.Println("Target job found for slug:", slug)
-				jobDetaByte, _ = json.Marshal(job)
-				break
-			}
-		}
-	}
-
-	if len(jobDetaByte) > 0 {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jobDetaByte)
-	} else {
+	ID, _ := strconv.ParseUint(splits[1], 10, 64)
+	foundJob, err := database.GetJobByID(uint(ID))
+	if err != nil {
+		logger.Error.Println("An error occurred while getting job by ID:", ID, "err:", err)
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("Job not found"))
+		return
 	}
+
+	job := rss.JobDTO{
+		Title:       foundJob.Title,
+		Description: foundJob.Description,
+		Region:      foundJob.Region,
+		Type:        foundJob.Type,
+		ApplyUrl:    foundJob.ApplyUrl,
+		Salary:      foundJob.Salary,
+		Date:        foundJob.PubDate,
+		Applicants:  foundJob.Applicant,
+		Company: rss.CompanyDTO{
+			Name:        foundJob.Company.Name,
+			Headquarter: foundJob.Company.Headquarter,
+			Url:         foundJob.Company.WebSite,
+			Logo:        foundJob.Company.Logo,
+		},
+	}
+
+	jobByte, err := json.Marshal(job)
+
+	if err != nil {
+		logger.Error.Println("An error occurred while marshalling job for job ID:", ID, "err:", err)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Job not found"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jobByte)
 }
 
-func JobCategoryHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, category string) {
-	logger.Info.Println("Getting jobs for category:", category)
+func JobCategoryHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, categorySlug string, database *db.Database) {
+	splits := strings.Split(categorySlug, "--")
 
-	var jobSummary []rss.JobSummary
-	var jobs rss.JobSummary
-	var job rss.Channel
-	jobData, _ := redis.GetJobs(ctx, category)
-	err := json.Unmarshal(jobData, &job)
+	logger.Info.Println("Getting jobs for category:", splits[0], "and cat ID:", splits[1])
+
+	catID, _ := strconv.ParseUint(splits[1], 10, 64)
+	jobsByCat, err := database.GetAllJobsByCatID(uint(catID))
+
 	if err != nil {
-		logger.Error.Println("An error occurred while unmarshalling cache data, err:", err)
-	} else if err == nil {
-		jobData, _ := redis.GetJobs(ctx, category)
-
-		var job rss.Channel
-		_ = json.Unmarshal(jobData, &job)
-		jobs.Jobs = []rss.JobsFields{}
-		for _, j := range job.Jobs {
-			jobFields := rss.JobsFields{
-				Title:    j.Title,
-				Company:  j.Company.Name,
-				Type:     j.Type,
-				Location: j.Region,
-				Date:     j.Date,
-				Logo:     j.Company.Logo,
-			}
-
-			jobs.Jobs = append(jobs.Jobs, jobFields)
-		}
-
-		jobs.Description = job.Description
-		jobSummary = append(jobSummary, jobs)
-
-		jobsByte, _ := json.Marshal(jobSummary)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jobsByte)
-	} else if jobData == nil {
+		logger.Error.Println("An error while getting category by ID:", catID, "err:", err)
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Job not found"))
+		w.Write([]byte("Jobs not found by category"))
+		return
 	}
+
+	var jobs rss.JobSummaryDTO
+	var jobSummary []rss.JobSummaryDTO
+	jobs.Jobs = []rss.JobsFieldsDTO{}
+	for _, j := range jobsByCat {
+		jobFields := rss.JobsFieldsDTO{
+			ID:       j.ID,
+			Title:    j.Title,
+			Company:  j.Company.Name,
+			Type:     j.Type,
+			Location: j.Region,
+			Date:     j.PubDate,
+			Logo:     j.Company.Logo,
+		}
+		jobs.Jobs = append(jobs.Jobs, jobFields)
+	}
+
+	jobs.CategoryName = jobsByCat[0].Category.Name
+	jobSummary = append(jobSummary, jobs)
+	jobsByte, err := json.Marshal(jobSummary)
+
+	if err != nil {
+		logger.Error.Println("An error while getting category by ID:", catID, "err:", err)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Jobs not found by category"))
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jobsByte)
 }
 
 func ServeBasicHtml(w http.ResponseWriter, r *http.Request) {
@@ -139,26 +147,14 @@ func ServeBasicHtml(w http.ResponseWriter, r *http.Request) {
 	w.Write(htmlFile)
 }
 
-func ApplyToJob(ctx context.Context, w http.ResponseWriter, r *http.Request, fullSlug string) {
-	slug := strings.Split(fullSlug, "--")[1]
-	err := redis.IncrementJobApplicantCount(ctx, slug)
+func ApplyToJob(ctx context.Context, w http.ResponseWriter, r *http.Request, fullSlug string, database *db.Database) {
+	slug := strings.Split(fullSlug, "--")
 
-	if err == nil {
-		logger.Info.Println("Applied to the job for slug:", slug)
+	jobID, _ := strconv.ParseUint(slug[1], 10, 64)
+	err := database.IncrementApplicant(uint(jobID))
+	if err != nil {
+		logger.Error.Println("An error occurred while increasing applicant count")
 	}
 
-	JobDetailsHandler(ctx, w, r, fullSlug)
-
-}
-
-func createSlug(title string) string {
-	slug := strings.ToLower(strings.TrimSpace(title))
-
-	reg := regexp.MustCompile(`[^\w\s-]`)
-	slug = reg.ReplaceAllString(slug, "")
-
-	reg = regexp.MustCompile(`[-\s]+`)
-	slug = reg.ReplaceAllString(slug, "-")
-
-	return slug
+	JobDetailsHandler(ctx, w, r, fullSlug, database)
 }
